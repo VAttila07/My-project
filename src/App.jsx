f24bfe17da72ecb1fb1c2bdabf8985c083f1f74c
VAttila07 <varkonyi.mc.attila@gmail.com>
2024-01-29 13:52:42 +0100

    shadCN import

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import React from 'react';
 import logo from './assets/Fade-background.png';
+import { Button } from './components/ui/button';
 
 const App = () => {
   return (
     <><h1 className='text-9xl uppercase underline font-thunder'>
       Hello world!
     </h1>
+    <Button>Hello Button</Button>
     <h1 className='text-9xl font-voyage'>
       Voyage
     </h1>
