45f233239cb795bc77d801e9d19d3caaa29b3f6b
VAttila07 <varkonyi.mc.attila@gmail.com>
2024-01-29 14:52:01 +0100

    Toggle Site

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,24 @@ import { Button } from './components/ui/button';
 import { ProgressDemo } from './components/ui/demo/progressDemo';
 
 const App = () => {
-  const [showSite, setShowSite] = useState(false);
+  const [showSite, setShowSite] = useState('hidden');
+  const [hideButton, setHideButton] = useState(' w-[100%] h-[100%]');
 
   const showButton = () => {
-    setShowSite(true);
+    setShowSite('');
+    setHideButton('opacity-0 hidden -z-100');
   }
 
   return (
     <div>
-      <Button onClick={showButton}>Begin</Button>
-      <div className=' sticky z-50 place-content-center'>
-        <ProgressDemo></ProgressDemo>
+      <Button onClick={showButton} className={hideButton}>Begin</Button>
+      <div className={showSite}>
+        <div className=' text-center place-content-center'>
+          Lorem ipsum
+          <div className=''>
+            <ProgressDemo></ProgressDemo>
+          </div>
+        </div>
       </div>
     </div>
   )
