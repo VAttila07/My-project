713c5ccbd153bd2c306b8d55aa881c0438fb1b47
VAttila07 <varkonyi.mc.attila@gmail.com>
2024-01-29 13:38:41 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import logo from './assets/Fade-background.png';
+
+const App = () => {
+  return (
+    <><h1 className='text-9xl uppercase underline font-thunder'>
+      Hello world!
+    </h1>
+    <h1 className='text-9xl font-voyage'>
+      Voyage
+    </h1>
+    </>
+  )
+}
+
+export default App
